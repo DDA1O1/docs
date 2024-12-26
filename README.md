c2abd6b93b6168a532faa55d73e1abbb095a7152
debashish <debashishdash03412@gmail.com>
2024-12-26 21:00:59 +0530

    Add initial project setup with Astro framework, including configuration files, .gitignore, and essential documentation. Introduced Tailwind CSS for styling and integrated various assets such as images and scripts. The project structure supports data science and web development documentation, enhancing educational resources with comprehensive guides and examples across multiple programming topics.

---
+++ README.md
@@ -0,0 +1,22 @@
+
+## 🚀 Project Structure
+
+```
+.
+├── public/
+├── src/
+│   ├── assets/
+│   ├── content/
+│   │   ├── docs/
+│   │   └── config.ts
+│   └── env.d.ts
+├── astro.config.mjs
+├── package.json
+└── tsconfig.json
+```
+
+Look for `.md` or `.mdx` files in the `src/content/docs/` directory. Each file is exposed as a route based on its file name.
+
+Images can be added to `src/assets/` and embedded in Markdown with a relative link.
+
+Static assets, like favicons, can be placed in the `public/` directory.
---
+++ astro.config.mjs
@@ -0,0 +1,95 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import vercel from '@astrojs/vercel';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+      title: 'Docs',
+      logo:{
+          src: './public/r.png',
+          alt: 'My Logo', // Add an alt attribute for selecting the logo
+      },
+      customCss:[
+          // Path to your Tailwind base styles:
+        './src/tailwind.css',
+        ],
+      social: {
+          github: 'https://github.com/DDA1O1',
+          twitter: 'https://twitter.com/DDA1O1',
+      },
+      editLink: {
+          baseUrl: 'https://github.com/DDA1O1/docs/tree/main',
+      },
+      sidebar: [
+          {
+              label: 'Guides',
+              items: [
+                  // Each item here is one entry in the navigation menu.
+                  { label: 'overview', slug: 'guides/overview' },
+              ],
+          },
+          {
+              label: 'Computer Architecture',
+              autogenerate: { directory: 'Computer_Architecture' },
+          },
+          {
+            label: 'Web Dev',
+            items: [
+              //   { label: 'HTML', slug: 'web_dev/html' }, 
+                { label: 'JavaScript' ,badge: 'Finished', autogenerate: { directory: 'web_dev/Javascript' } },
+                { label: 'CSS' , autogenerate: { directory: 'web_dev/CSS' } },
+            ]
+          },
+          {
+              label: 'C++',
+              autogenerate: { directory: 'C++' },
+          },
+          {
+              label: 'Python',
+              autogenerate: { directory: 'python' }
+          },
+          {
+              label: 'Data Science',
+              autogenerate: { directory: 'data_science' }
+          },
+          {
+              label: 'DSA',
+              autogenerate: { directory: 'DSA' }
+          },
+          {
+              label: 'Thoughts',
+              autogenerate: { directory: 'Thoughts' }
+          },
+          {
+              label: 'Fun',
+              items: [
+                {
+                  label: 'Lorentz',
+                  link: 'https://lorentz.d1o1.fun/',
+                  attrs: { target: '_blank', style: 'font-style: italic' },
+                },
+              ]
+          },
+      ],
+  }), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false,
+  })],
+
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+  vite: {
+    resolve: {
+      alias: {
+        '@/': '/src/'
+      }
+    }
+  }
+});
