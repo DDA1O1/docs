41255e2becef6e4ce597e5797faed8b47b7daa72
debashish <debashishdash03412@gmail.com>
2025-01-04 23:11:19 +0530

    fixing error

--- public/disableClick.js
+++ public/disableClick.js
@@ -1,3 +1,14 @@
-document.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-  });
+document.addEventListener('keydown', function(e) {
+    // Disable F12
+    if (e.key === 'F12') {
+        e.preventDefault();
+    }
+    // Disable Ctrl+Shift+I (Windows) / Cmd+Option+I (Mac)
+    if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.key === 'I') {
+        e.preventDefault();
+    }
+    // Disable Ctrl+U (View Source)
+    if ((e.ctrlKey || e.metaKey) && e.key === 'u') {
+        e.preventDefault();
+    }
+});
